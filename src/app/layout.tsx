52f5a09d5959e4fdb3f582c0e910d2b242531e10
Pedro Jorge <pedrojorge2011@gmail.com>
2024-05-16 08:57:36 -0300

    setup react query and get accounts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
+import { QueryProvider } from '@/providers/query-provider'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -28,7 +29,7 @@ export default function RootLayout({
             fontSans.variable,
           )}
         >
-          {children}
+          <QueryProvider>{children}</QueryProvider>
         </body>
       </html>
     </ClerkProvider>
