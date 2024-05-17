76453b532fb9e8175a076b74234bcc8ecc404f81
Pedro Jorge <pedrojorge2011@gmail.com>
2024-05-16 23:17:00 -0300

    feat: data-table and bulk delete

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -3,8 +3,9 @@ import { accounts, insertAccountSchema } from '@/db/schema'
 import { zValidator } from '@hono/zod-validator'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
-import { eq } from 'drizzle-orm'
+import { and, eq, inArray } from 'drizzle-orm'
 import { Hono } from 'hono'
+import { z } from 'zod'
 
 const app = new Hono()
   .get('/', clerkMiddleware(), async (c) => {
@@ -47,6 +48,31 @@ const app = new Hono()
           userId: auth.userId,
           name,
         })
+        .returning()
+
+      return c.json({ data })
+    },
+  )
+  .post(
+    '/bulk-delete',
+    clerkMiddleware(),
+    zValidator(
+      'json',
+      z.object({
+        ids: z.array(z.string()),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c)
+      const { ids } = c.req.valid('json')
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized.' }, 401)
+      }
+
+      const data = await db
+        .delete(accounts)
+        .where(and(eq(accounts.userId, auth.userId), inArray(accounts.id, ids)))
         .returning()
 
       return c.json({ data })
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import { ClerkLoaded, ClerkLoading, UserButton } from '@clerk/nextjs'
 
 export function Header() {
   return (
-    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 pb-16 lg:px-14">
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 pb-36 lg:px-14">
       <div className="mx-auto max-w-screen-2xl">
         <div className="mb-14 flex w-full items-center justify-between">
           <div className="flex items-center lg:gap-16">
