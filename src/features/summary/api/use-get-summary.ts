e19f8fd79027f6189812dd17eee1b98adc185260
Pedro Jorge <pedrojorge2011@gmail.com>
2024-05-31 15:00:41 -0300

    feat: data cards on dashboard page

---
+++ src/features/summary/api/use-get-summary.ts
@@ -0,0 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { client } from '@/lib/hono'
+import { convertAmountFromCents } from '@/lib/utils'
+import { useSearchParams } from 'next/navigation'
+
+export function useGetSummary() {
+  const params = useSearchParams()
+
+  const from = params.get('from') || ''
+  const to = params.get('to') || ''
+  const accountId = params.get('accountId') || ''
+
+  const query = useQuery({
+    // TODO: Check if params are needed in the key
+    queryKey: ['summary', { from, to, accountId }],
+    queryFn: async () => {
+      const response = await client.api.summary.$get({
+        query: {
+          from,
+          to,
+          accountId,
+        },
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch summary.')
+      }
+
+      const { data } = await response.json()
+      return {
+        ...data,
+        incomeAmount: convertAmountFromCents(data.incomeAmount),
+        expensesAmount: convertAmountFromCents(data.expensesAmount),
+        remainingAmount: convertAmountFromCents(data.remainingAmount),
+        categories: data.categories.map((category) => ({
+          ...category,
+          value: convertAmountFromCents(category.value),
+        })),
+        days: data.days.map((day) => ({
+          ...day,
+          income: convertAmountFromCents(day.income),
+          expenses: convertAmountFromCents(day.expenses),
+        })),
+      }
+    },
+  })
+
+  return query
+}
